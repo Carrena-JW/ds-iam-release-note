5ed148aaab538388e1e728c8fb7e5206298ccc3f
102boy <carrena1642@gmail.com>
2025-03-18 22:53:00 +0900

    chore: add icon

--- src/iam-release-note/src/app/app.component.ts
+++ src/iam-release-note/src/app/app.component.ts
@@ -4,8 +4,10 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  template: `
+    <router-outlet></router-outlet>
+  `,
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'iam-release-note';
--- src/iam-release-note/src/app/pages/home/home.component.ts
+++ src/iam-release-note/src/app/pages/home/home.component.ts
@@ -1,13 +1,23 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
     selector: 'app-home',
     standalone: true,
     imports: [
         CommonModule,
+        MatIconModule
     ],
-    template: `<p>home works!</p>`,
+    template: `
+<mat-icon color="" aria-hidden="false" aria-label="Example home icon" fontIcon="home"></mat-icon>
+
+        <div class="main-banner">
+    
+        <p class="sub-word">Racoon.</p>
+            <p class="main-word">Release Notes</p>
+        </div>
+    `,
     styleUrl: './home.component.css',
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
